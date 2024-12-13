5e07213f10f31e00ba9978c5fc0545b23f57e552
vishal <vishalpawar2550@gmail.com>
2024-12-13 20:10:45 +0530

    added Service  article

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 AOS.init();
+// show data
+function setCondition(condition) {
+  // Hide all sections
+  document.querySelectorAll('.section').forEach(section => section.classList.add('hidden'));
+
+  // Show the selected section
+  const targetSection = document.getElementById(condition);
+  targetSection.classList.remove('hidden');
+
+  // Scroll to the visible section
+  targetSection.scrollIntoView({
+    behavior: 'smooth', // Smooth scrolling
+    block: 'start'      // Align to the top of the viewport
+  });
+}
